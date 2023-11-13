b5ce4481418a832eac89c503c8ff8e271af0068f
Kamil Wenta <kamil.wenta222@gmail.com>
2023-11-13 01:07:16 +0100

    Update drizzle-orm to 0.29.0

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,10 @@
-import { type Config } from "drizzle-kit";
-
 import { env } from "@/env.mjs";
+import { type Config } from "drizzle-kit";
 
 export default {
   schema: "./src/server/db/schema.ts",
   driver: "mysql2",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    uri: env.DATABASE_URL,
   },
 } satisfies Config;
